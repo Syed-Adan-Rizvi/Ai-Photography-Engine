8625aeb014f5399cc03f3eb116e158a0e7bc2fd4
Syed Adan Rizvi <adanrizvihfd@gmail.com>
2026-05-04 01:14:38 +0500

    vite version fixed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'force-close-vercel-build',
+      apply: 'build',
+      closeBundle() {
+        process.exit(0)
+      }
+    }
+  ]
 })
