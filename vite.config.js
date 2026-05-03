4333cc5a6c07d19f082a57a583684eae4fb37df6
Syed Adan Rizvi <adanrizvihfd@gmail.com>
2026-05-04 01:44:45 +0500

    fix: remove process.exit plugin from vite config

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    react(),
-    {
-      name: 'force-close-vercel-build',
-      apply: 'build',
-      closeBundle() {
-        process.exit(0)
-      }
-    }
-  ]
+    react()
+  ],
+  build: {
+    outDir: 'dist'
+  }
 })
